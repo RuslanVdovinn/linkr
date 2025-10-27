321a8a07ff74ee132eb9358ddf07cea3e0aebaa1
Ruslan Vdovin <vdovin@mgalaxy.pro>
2025-10-27 12:25:54 +0700

    server

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,19 +1,61 @@
 package main
 
 import (
+	"context"
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	"linkr/internal/migrations"
 	"log"
+	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/joho/godotenv"
 
+	"github.com/go-chi/chi/v5"
 	_ "github.com/lib/pq"
 )
 
 func main() {
 	godotenv.Load()
+	migrate()
+	addr, ok := os.LookupEnv("HTTP_ADDR")
+	if !ok {
+		addr = ":8080"
+	}
+	srv := &http.Server{
+		Addr:    addr,
+		Handler: routes(),
+	}
+	go func() {
+		log.Printf("listening on %s", addr)
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("server error: %v", err)
+		}
+	}()
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	srv.Shutdown(ctx)
+	log.Println("server stopped")
+}
+
+func routes() http.Handler {
+	r := chi.NewRouter()
+	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(map[string]string{"status": "ok"})
+	})
+	return r
+}
+
+func migrate() {
 	dbName, ok := os.LookupEnv("DB_USER")
 	if !ok {
 		log.Println("Missing DB_USER")
@@ -24,7 +66,7 @@ func main() {
 		log.Println("Missing DB_PASSWORD")
 		os.Exit(1)
 	}
-	db, err := sql.Open("postgres", fmt.Sprintf("postgres://%s:%s@localhost:32768/linkr?sslmode=disable", dbName, dbPassword))
+	db, err := sql.Open("postgres", fmt.Sprintf("postgres://%s:%s@localhost:32768/postgres?sslmode=disable", dbName, dbPassword))
 	if err != nil {
 		panic(err)
 	}
--- go.mod
+++ go.mod
@@ -10,6 +10,7 @@ require (
 )
 
 require (
+	github.com/go-chi/chi/v5 v5.2.3
 	github.com/golang-migrate/migrate/v4 v4.19.0
 	github.com/lib/pq v1.10.9
 )
